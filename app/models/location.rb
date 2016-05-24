9beb9a17854ed821e6aa2c776e80110d833dcc21
Kait Austin <kait.t.austin@gmail.com>
2016-05-23 21:00:30 -0500

    Setup New and Show View

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  geocoded_by: full_street_address
+  geocoded_by :full_street_address
   after_validation :geocode #auto fetch the coordinates from geocoder
 
   def full_street_address
